64dc1b55ca7ba187528c0b8b162533cddd9495cf
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-04-26 17:08:27 +0530

    Create ForLoop.java

---
+++ src/ForLoop.java
@@ -0,0 +1,21 @@
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		/*
+		 * Syntax
+		 * for(init;condition;updation)
+		 * {}
+		 */
+		
+		for(int i=1; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		//while loop : don't know the number of iterations
+		// Do while loop : don't know the number of iterations but want to execute the code at least once irrespective of whatever the conditions
+		// For Loop : you know the number of iterations
+
+	}
+
+}
