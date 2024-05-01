3b1c31a12ebf63eaef7633d2d97b04a4307816c8
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-01 21:57:39 +0530

    Create DataTypes.java

---
+++ src/DataTypes.java
@@ -0,0 +1,29 @@
+
+public class DataTypes {
+	public static void main(String[] args) {
+		/*
+		 * Datatypes
+		 * 1. Primitive Datatypes
+		 *		- byte : used to store number between -128 to 127
+		 *		- short
+		 *		- int
+		 *		- long
+		 *		- float
+		 *		- double
+		 *		- boolean
+		 *		- char
+		 *
+		 * 2. Non Primitive Datatypes
+		 * 		- String
+		 * 		- Arrays
+		 * 
+		 */
+		byte demo = 10;
+		System.out.println(demo);	
+		
+		short demo2 = 3492;
+		System.out.println(demo2);
+		
+	}
+
+}
