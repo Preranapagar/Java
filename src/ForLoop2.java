0585e2d4272907aae72f46d25de7f34e8d65b0a6
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-03 14:40:53 +0530

    Create ForLoop2.java

---
+++ src/ForLoop2.java
@@ -0,0 +1,15 @@
+
+public class ForLoop2 {
+
+	public static void main(String[] args) {
+		// print 40 to 60 numbers except 44,48 and 56
+		
+		for (int i = 40; i<=60; i++)
+		{	
+			if((i==44)&&())
+			System.out.println(i);
+		}
+
+	}
+
+}
