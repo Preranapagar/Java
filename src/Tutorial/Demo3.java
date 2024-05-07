8e720206e023ffdbe6cce34ca207946689724b53
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 22:41:02 +0530

    Create Demo3.java

---
+++ src/Tutorial/Demo3.java
@@ -0,0 +1,17 @@
+package Tutorial;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// print 90 to 80 values using while loop
+		
+		int i = 90;
+		while (i>=80)
+		{
+			System.out.println(i);
+			i--;
+		}
+
+	}
+
+}
