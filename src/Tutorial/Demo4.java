d365fe102a2474a348aa57ed569b1c31a46b2e0c
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 22:41:04 +0530

    Create Demo4.java

---
+++ src/Tutorial/Demo4.java
@@ -0,0 +1,20 @@
+package Tutorial;
+
+public class Demo4 {
+
+	public static void main(String[] args) {
+		// print 40 to 50 even values using while loop
+		
+		int i = 40;
+		while (i<=50)
+		{
+			if(i%2==0)
+			{
+				System.out.println(i);
+			}
+			i++;
+		}
+
+	}
+
+}
