0db5f7880751c69335e1e69e0e554ab398e4caff
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 13:30:52 +0530

    Create AssignmentOperator.java

---
+++ src/AssignmentOperator.java
@@ -0,0 +1,35 @@
+
+public class AssignmentOperator {
+
+	public static void main(String[] args) {
+		//AssignmentOperator
+		/*
+		 * Assignment (=)
+		 * Add and Assign (+=)
+		 * Sub and Assign (-=)
+		 * Div and Assign (/=)
+		 * Mul and Assign (*=)
+		 * Module and Assign (%=)
+		 */
+		
+		int a = 100;
+		System.out.println(a);
+		
+		a+=2;
+		System.out.println(a);
+		
+		a-=70;
+		System.out.println(a);
+		
+		a/=3;
+		System.out.println(a);
+		
+		a*=6;
+		System.out.println(a);
+		
+		a%=3;
+		System.out.println(a);
+
+	}
+
+}
