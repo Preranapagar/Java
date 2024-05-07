e319d7927b298ae40f2452b0262537506398d30e
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 13:30:58 +0530

    Create RelationalOperator.java

---
+++ src/RelationalOperator.java
@@ -0,0 +1,28 @@
+
+public class RelationalOperator {
+
+	public static void main(String[] args) {
+		//Relational Operator
+		/*
+		 * equal to (==)
+		 * not equal to (!=)
+		 * greater than (>)
+		 * greater than equal to (>=)
+		 * less than (<)
+		 * less than equal to (<=)
+		 */
+		
+		int a = 34;
+		int b = 46;
+		
+		System.out.println(a==b);
+		System.out.println(a!=b);
+		System.out.println(a>b);
+		System.out.println(a>=b);
+		System.out.println(a<b);
+		System.out.println(a<=b);
+		
+
+	}
+
+}
