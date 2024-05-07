5bea6a343f621e6818e1353b25da750fe069cd07
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 13:30:56 +0530

    Update Operators.java

--- src/Operators.java
+++ src/Operators.java
@@ -1,9 +1,59 @@
 
+
 public class Operators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Operators in Java
+		 * 1. Arithmetic Operator
+		 * 2. Relational Operator
+		 * 3. Logical Operator
+		 * 4. Assignment Operator
+		 */
+		
+		//Arithmetic Operator
+		// 1. Addition (+)
+		// 2. Subtraction (-)
+		// 3. Multiplication (*)
+		// 4. Division (/)
+		// 5. Module (%)
+		// 6. Increment (++)
+		// 7. Decrement (--)
+		
+		//eg.
+		
+		int a = 10, b= 20;
+		
+		System.out.println(a+b);
+		System.out.println(b-a);
+		System.out.println(a*b);
+		System.out.println(b/a);
+		System.out.println(b%a);
+		
+		a++;
+		System.out.println(a);
+		
+		b--;
+		System.out.println(b);
+		
+		//eg.
+		
+		int c=5;
+		float d=2.5f;
+		
+		System.out.println(c+d);
+		System.out.println(c-d);
+		System.out.println(c*d);
+		System.out.println(c/d);
+		System.out.println(c%d);
+		
+		d++;
+		System.out.println(d);
+		
+		
+		
 
 	}
-
+	
 }
+ 
