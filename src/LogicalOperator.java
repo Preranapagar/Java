ed2344b756c48e471283c0730033191027993cd9
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 13:30:54 +0530

    Create LogicalOperator.java

---
+++ src/LogicalOperator.java
@@ -0,0 +1,30 @@
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// Logical Operator
+		/*
+		 * and operator (&&)
+		 * or operator (||)
+		 * not operator (!)
+		 */
+		
+		//and operator (&&)
+		System.out.println(true&&true);
+		System.out.println(true&&false);
+		System.out.println(false&&true);
+		System.out.println(false&&false);
+		
+		//or operator (||)
+		System.out.println(true||true);
+		System.out.println(false||true);
+		System.out.println(false||false);
+		System.out.println(true||false);
+		
+		//not operator
+		System.out.println(!true);
+		System.out.println(!false);
+
+	}
+
+}
