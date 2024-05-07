d08e3a4cfd43c7ba5d72ffd621b6a93eae120449
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-07 22:40:57 +0530

    Create Demo1.java

---
+++ src/Tutorial/Demo1.java
@@ -0,0 +1,18 @@
+package Tutorial;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// print 1 to 5 values using while loop
+		
+		int i = 1;
+		
+		while (i<=5)
+		{
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
