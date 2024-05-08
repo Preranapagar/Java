651076e2e06c1dfa1d6635d8308a3d887e8fa138
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2024-05-08 15:09:52 +0530

    string

---
+++ src/StringTutorials/Charat.java
@@ -0,0 +1,16 @@
+package StringTutorials;
+
+public class Charat {
+
+	public static void main(String[] args) {
+		// use to capture character by  index position
+		// return type is char datatype
+		
+		String a = "Pune";
+		
+		char b = a.charAt(2);
+		System.out.println(b);
+
+	}
+
+}
