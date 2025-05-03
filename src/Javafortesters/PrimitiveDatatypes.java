a7fcc840560b10890616f5804fdbe7bc6f1345a0
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-05-03 14:08:16 +0530

    update

---
+++ src/Javafortesters/PrimitiveDatatypes.java
@@ -0,0 +1,60 @@
+package Javafortesters;
+
+public class PrimitiveDatatypes {
+	public static void main(String[] args) {
+		
+		//Primitive Datatypes
+		
+		//1.byte : it is used to store numbers between -128 to 127 values.
+
+		byte abc = 10;
+		
+		System.out.println(abc);
+		
+		//2.short : it is used to store number between -32768 to 32767
+		
+		short nsk = 30000;
+		System.out.println(nsk);
+		
+		//3.int :It is used to store number between -2,147,483,648 to 2,147,483,647
+		
+		int az = 234545671;
+		System.out.println(az);
+		
+		//4. long : It is used to store values between -9,223,372,036,854,775,808 to 9,223,372,038,775,807
+		//If we have large values then at end of values we use upper case L or lower case l.
+		
+		long we = 67950284054822l;
+		System.out.println(we);
+
+		
+		//5.float - it used to store the decimal values between 6 to 7 digits
+		//at the end of values we have use upper case F or lower case f.
+		
+		float kyc = 13.4f;
+		System.out.println(kyc);
+		
+		//6. double : It is used to store decimal values up to 15 digits
+		
+		double er = 45.677863;
+		System.out.println(er);
+		
+		//7. Boolean : To store Boolean values like True/False
+		
+		boolean yes = true;
+		boolean no = false;
+		
+		System.out.println(yes+" "+no);
+		
+		//8. char : To store a single character using single quotation
+		
+		char key = 'k';
+		System.out.println(key);
+		
+	
+		
+		
+	
+	}
+
+}
