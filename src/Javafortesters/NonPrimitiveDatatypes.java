3f06ee7cb1f6d8ce4b444f7ef855f559e5f7621e
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2025-05-06 21:35:36 +0530

    Create NonPrimitiveDatatypes.java

---
+++ src/Javafortesters/NonPrimitiveDatatypes.java
@@ -0,0 +1,23 @@
+package Javafortesters;
+
+public class NonPrimitiveDatatypes {
+	
+	public static void main (String[] args) {
+		
+		// Non Primitive Datatypes
+		
+		/*
+		 * • string : - String is sequence of character written in double quote - String
+		 * may have numbers, upper case character, lower case character and special
+		 * character
+		 */
+		
+			String a = "I am learning java";
+			System.out.println(a);
+		
+		
+
+		
+	}
+
+}
