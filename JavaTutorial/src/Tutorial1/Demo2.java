b0c78258c4183036aa9a70ed93d34555ec140790
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2026-01-24 11:52:02 +0530

    Create Demo2.java

---
+++ JavaTutorial/src/Tutorial1/Demo2.java
@@ -0,0 +1,19 @@
+package Tutorial1;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		
+		byte a = 100;
+		short b = 2244;
+		int c = 345554;
+		long d = 23456789L;
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		
+	}
+
+}
