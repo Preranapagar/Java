b308cac1658c3ff57b91da0ee4e7fc2eecfa10f3
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2026-01-25 19:36:55 +0530

    update

--- JavaTutorial/src/Tutorial5/Demo9.java
+++ JavaTutorial/src/Tutorial5/Demo9.java
@@ -3,7 +3,7 @@ package Tutorial5;
 public class Demo9 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
---
+++ RestAssuredTutorial/src/test/java/Tutorial1/Demo1.java
@@ -0,0 +1,14 @@
+package Tutorial1;
+
+import io.restassured.RestAssured;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		RestAssured.baseURI = "http://localhost:3000";
+		
+
+	}
+
+}
