a591542d3b4d013daaf8614957020c12626b8bc3
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2026-01-27 10:26:07 +0530

    update

--- JavaTutorial/src/Tutorial5/Demo9.java
+++ JavaTutorial/src/Tutorial5/Demo9.java
@@ -4,6 +4,10 @@ public class Demo9 {
 
 	public static void main(String[] args) {
 		
+		String a = "Cucumber";
+		
+		char[] abc = a.toCharArray();
+		
 
 	}
 
---
+++ JavaTutorial/src/Tutorial7/Demo1.java
@@ -0,0 +1,16 @@
+package Tutorial7;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		char a1 = 'p';
+		char a2 = '1';
+		
+		System.out.println(Character.isLetter(a1));
+		System.out.println(Character.isLetter(a2));
+		
+
+	}
+
+}
