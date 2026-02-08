62d5dd809ad855fcf89f2c885764c601c44d8e33
Preranapagar <104857193+Preranapagar@users.noreply.github.com>
2026-02-08 14:18:01 +0530

    Create Demo2.java

---
+++ JavaTutorial/src/Sets/Demo2.java
@@ -0,0 +1,32 @@
+package Sets;
+
+import java.util.LinkedHashSet;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		LinkedHashSet s1 = new LinkedHashSet();
+		
+		s1.add(100);
+		s1.add("selenium");
+		s1.add(true);
+		s1.add(100);
+		s1.add(100);
+		s1.add(null);
+		s1.add(null);
+		s1.add("rest");
+		
+		System.out.println(s1);
+		
+		boolean a1 = s1.remove("rest");
+		
+		System.out.println(a1);
+		
+		boolean a2 = s1.contains(null);
+		
+		System.out.println(a2);
+
+	}
+
+}
